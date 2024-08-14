3841cda120529be90e77100b8e9eca35a7130342
Francesco Calvi <calvi.fra@gmail.com>
2024-08-14 10:49:16 +0200

    error handling for subscription

--- src/components/plans/SubscribeButton.tsx
+++ src/components/plans/SubscribeButton.tsx
@@ -1,17 +1,17 @@
-import React from "react";
-import { SubscribeButton as Button } from "./Plans.style";
-import db from "../../firebase";
+import { loadStripe } from "@stripe/stripe-js";
 import {
+  addDoc,
   collection,
   doc,
-  addDoc,
-  onSnapshot,
   DocumentData,
+  onSnapshot,
 } from "firebase/firestore";
+import React from "react";
 import { useSelector } from "react-redux";
-import { selectUser } from "../../state/features/userSlice";
-import { loadStripe } from "@stripe/stripe-js";
 import { Spinner } from "../../App.style";
+import db from "../../firebase";
+import { selectUser } from "../../state/features/userSlice";
+import { SubscribeButton as Button } from "./Plans.style";
 
 const SubscribeButton: React.FC<{
   priceId: string;
@@ -26,33 +26,38 @@ const SubscribeButton: React.FC<{
     setIsLoading(true);
     onCheckoutRequestFired(true);
 
-    const docRef = doc(db, "customers", user.uid);
-    const checkOutRef = collection(docRef, "checkout_sessions");
-    const sessionRef = await addDoc(checkOutRef, {
-      price: priceId,
-      success_url: window.location.origin,
-      cancel_url: window.location.origin,
-    });
+    try {
+      const docRef = doc(db, "customers", user.uid);
+      const checkOutRef = collection(docRef, "checkout_sessions");
+      const sessionRef = await addDoc(checkOutRef, {
+        price: priceId,
+        success_url: window.location.origin,
+        cancel_url: window.location.origin,
+      });
 
-    onSnapshot(sessionRef, async (snap: DocumentData) => {
-      const { error, sessionId } = snap.data();
+      onSnapshot(sessionRef, async (snap: DocumentData) => {
+        const { error, sessionId } = snap.data();
 
-      if (error) {
-        alert(`An error occured: ${error.message}`);
-        setIsLoading(false);
-      }
-      if (sessionId) {
-        // We have a session, let's redirect to Checkout
-        // Init Stripe
-        const stripe = await loadStripe(
-          "pk_test_51LfUqDLFC3vmHF5tcl8HtSdwKdOLDzwMUx0dwWTMxpTb9EDI6tiKicLedo1t9MtV05S6XhI3uZ2w0KSxzXENJBBi00GJBplTRU"
-        );
+        if (error) {
+          throw new Error(`An error occured: ${error.message}`);          
+        }
+        if (sessionId) {
+          // We have a session, let's redirect to Checkout
+          // Init Stripe
+          const stripe = await loadStripe(
+            "pk_test_51LfUqDLFC3vmHF5tcl8HtSdwKdOLDzwMUx0dwWTMxpTb9EDI6tiKicLedo1t9MtV05S6XhI3uZ2w0KSxzXENJBBi00GJBplTRU"
+          );
 
-        stripe?.redirectToCheckout({
-          sessionId,
-        });
-      }
-    });
+          stripe?.redirectToCheckout({
+            sessionId,
+          });
+        }
+      });
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   return (
